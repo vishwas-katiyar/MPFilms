d73c4e039eb202ec2df60d492a1e2ac2170c6c67
Vishwas Katiyar <Vishwas Katiyar@inindcrws.com>
2023-07-21 22:09:19 +0530

    added

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -36,16 +36,24 @@ export default class Document extends NextDocument {
     return (
       <Html>
         <head>
-          {/* <meta charSet="UTF-8" /> */}
           <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
           <title>MP Films | Line Production in Madhya Pradesh</title>
           <script
             dangerouslySetInnerHTML={{
-              __html: `function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-KTH29NK8');`,
+              __html: `function hello(w, d, s, l, i) {
+  w[l] = w[l] || [];
+  w[l].push({
+    'gtm.start': new Date().getTime(),
+    event: 'gtm.js'
+  });
+  var f = d.getElementsByTagName(s)[0];
+  var j = d.createElement(s);
+  var dl = l !== 'dataLayer' ? '&l=' + l : '';
+  j.async = true;
+  j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+  f.parentNode.insertBefore(j, f);
+}
+hello(window, document, 'script', 'dataLayer', 'GTM-KTH29NK8');`,
             }}
           />
           <meta
@@ -105,7 +113,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <meta name="theme-color" content="#000000"></meta>
-          <script src="https://cdn.tailwindcss.com"></script>
+          <script src="https://cdn.tailwindcss.com/3.3.0"></script>
           <script
             dangerouslySetInnerHTML={{
               __html: `(${setCssVariables.toString()})({variables:${JSON.stringify(VARIABLES)}})`,
