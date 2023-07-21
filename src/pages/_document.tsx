85e1b8a413f10a9d31e312077b88ea4ba637826b
Vishwas Katiyar <Vishwas Katiyar@inindcrws.com>
2023-07-21 20:49:26 +0530

    meta

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -37,7 +37,7 @@ export default class Document extends NextDocument {
       <Html>
         <head>
           <meta charSet="UTF-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
           <title>MP Films | Line Production in Madhya Pradesh</title>
           <script
             dangerouslySetInnerHTML={{
